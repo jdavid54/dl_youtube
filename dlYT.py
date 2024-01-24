a6297191581d1f3ee8edf2c6015a8cd563a31c51
Jean David <jeandavid54@aol.com>
2024-01-24 22:06:07 +0100

    Update dlYT.py

--- dlYT.py
+++ dlYT.py
@@ -53,9 +53,7 @@ def download_video(url, resolution, file_size):
     progress_bar.close()
     print("complete download")
 
-# url:str ="https://www.youtube.com/watch?v=l4SjSHlxXOQ"   # put link video here
-
-url:str ="https://www.youtube.com/watch?v=WOEO1UuCRcc"
+# url:str ="https://www.youtube.com/watch?v.........."   # put link video here
 resolution, file_size = get_video_info(url)
 #print(resolution, file_size)
 
