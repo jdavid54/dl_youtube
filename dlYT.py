ed53d1556ee33aaf0308a2047567191d0103e9a7
Jean David <jeandavid54@aol.com>
2024-02-14 22:17:20 +0100

    Add files via upload

--- dlYT.py
+++ dlYT.py
@@ -1,10 +1,11 @@
-
+#https://pytube.io/en/latest/user/playlist.html
 import requests
-from pytube import YouTube
+from pytube import YouTube, Playlist
 from tqdm import tqdm
+from time import sleep
 
 # take reso title and video size
-def get_video_info(url):
+def get_video_info(url, display=True):
     video = YouTube(url)
     title = video.title
     #resolution = video.streams.get_lowest_resolution()
@@ -13,11 +14,36 @@ def get_video_info(url):
     #print(video.streams)
     response = requests.head(resolution.url)
     file_size = int(response.headers.get('content-length', 0))  # return 0 if key not found
-    #print(response.headers)  # use help
-    print(f"video name : {title}")
-    print(f"reso : {resolution.resolution}")
-    print(f"video size : {file_size / (1024 * 1024):.2f} MB")
-    return resolution, file_size
+    if file_size>0 and display:
+        #print(response.headers)  # use help
+        print(f"video name : {title}")
+        print(f"reso : {resolution.resolution}")
+        print(f"video size : {file_size / (1024 * 1024):.2f} MB")
+    return title, resolution, file_size
+
+def get_playlist_info(url):
+    p = Playlist(url)
+    for i,k in enumerate(p):
+        print(i+1,k)
+        try:
+            title, resolution, file_size = get_video_info(k)                          
+        except:
+            pass
+
+def save_playlist(url):
+    p = Playlist(url)
+    id = url.split('list=')[1]
+    buffer = ''
+    for i,k in enumerate(p):
+        try:
+            title, resolution, file_size = get_video_info(k,False)
+            print(title)
+            buffer += f'{title} ({file_size / (1024 * 1024):.2f} MB)\n'
+        except:
+            pass
+    with open(f'{id}.txt','w',encoding='utf8',errors='ignore') as f:
+        f.write(buffer)
+    
 
 #download with progress bar
 def download_video(url, resolution, file_size):
@@ -53,13 +79,50 @@ def download_video(url, resolution, file_size):
     progress_bar.close()
     print("complete download")
 
-url:str ="https://www.youtube.com/watch?v.........."   # put link video here
-resolution, file_size = get_video_info(url)
-#print(resolution, file_size)
+def download_playlist(url):
+    p = Playlist(url)
+    print(p)
+    for i,k in enumerate(p):
+        print(i+1,k)
+        try:
+            title, resolution, file_size = get_video_info(k)
+            print(file_size)
+            #sleep(5)
+            if file_size>0:
+                print('Downloading...')
+                download_video(k, resolution, file_size)                
+        except:
+            pass
 
-print('Download ?')
-input()
-if file_size!=0:
-    download_video(url, resolution, file_size)
+
+
+#========DOWLOAD=========================================
+def download(url):
+    # url:str ="https://www.youtube.com/watch?v=l4SjSHlxXOQ"   # put link video here
+    #url:str ="https://www.youtube.com/watch?v=WOEO1UuCRcc"
+    #url:str = "https://www.youtube.com/watch?v=CDonLjhYj28"
+    file_size=0
+    while file_size==0:
+        title, resolution, file_size = get_video_info(url)
+        #print(resolution, file_size)
+
+    print(f'Download Y/n ?')
+    if input()=='n':return
+
+    if file_size>0:
+        download_video(url, resolution, file_size)
 
 # help(video.streams)
+
+# download 1 video
+# download(url)
+
+
+#===========PLAYLIST=====================================
+url:str='https://www.youtube.com/playlist?list=PLy_OaHcu9ip98SNRy8qH4hXp4o76C5Y_6'
+url = 'https://www.youtube.com/playlist?list=PLKVMH2x-Imghzdb42BruShPgjY4Ghj7nN'
+# get list info   
+#get_playlist_info(url)
+
+# download playlist
+#download_playlist(url)
